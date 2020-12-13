922fbc90a19cf136e18a684fbc8fbb534265f281
Alanneilon <alanneilon@gmail.com>
2020-12-13 10:24:32 +0000

    Added Function to allow a player to make a move

--- tictac.py
+++ tictac.py
@@ -1,9 +1,21 @@
 def showBoard(board):
     print(board)
 
+def playerMove():
+    move=''
+    while move=='':
+        try:
+            move=int(input("Please enter a number between 1 and 9 :"))
+        except:
+            print ("Incorrect ")
+            continue
+        return move
+
 board=[0,1,2,3,4,5,6,7,8]
 
 showBoard(board)
+
+playerMove()
 
 
 
