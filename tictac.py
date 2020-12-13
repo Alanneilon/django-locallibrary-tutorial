125f2f5a0c718614e9401ceae25e868a35f255fc
Alanneilon <alanneilon@gmail.com>
2020-12-13 23:47:41 +0000

    Added check to see if spot is taken

--- tictac.py
+++ tictac.py
@@ -16,16 +16,20 @@ def playerChoice():
     while True:
         playerChoice=playerMove()
         if 0<playerChoice<10:
-            print("Move Accepted")
             enterChoiceOnBoard(playerChoice)
+            print("Move Accepted ")
             break
         print ("Invalid number range entered: ")
     return playerChoice
 
 def enterChoiceOnBoard(choice):
-    board[choice-1]=choice
+    if board[choice-1]=='X':
+        print("This position is taken ")
+        playerChoice()
+    else:
+        board[choice-1]='X'
 
-board=[0,1,2,3,4,5,6,7,8]
+board=[0,1,'X',3,4,5,6,7,8]
 
 showBoard(board)
 
