c3bdbfbd9688f0a5f73f52635dfd22786123d07d
Alanneilon <alanneilon@gmail.com>
2020-12-19 12:20:46 +0000

    Finished winnerCheck

--- tictac.py
+++ tictac.py
@@ -38,18 +38,91 @@ def playGame():
             player="Player 1"
         else:
             player="Player 2"
+        showBoard(board)
         playerChoice(player)
         showBoard(board)
-        print("Your Turn")
+        if checkWinner(board)==True:
+            break
+        else:
+            print("Your Turn")
         print(player)
         count+=1
 ###############################################################################################################
 def checkWinner(board):
     winner=False
-    if board[0]=='X' and board[1]=='X' and board[2]=='X':
+    while winner==False:
+        if board[0]=='X' and board[1]=='X' and board[2]=='X':
+            print("Well done, player1. You won")
+            winner=True
+            break
+        elif board[0]=='O' and board[1]=='O' and board[2]=='O':
+            print("Well done, player 2. You won!")
+            winner=True
+            break
+        elif board[3]=='X' and board[4]=='X' and board[5]=='X':
+            print("Well done, player 1. You won")
+            winner=True
+            break
+        elif board[3]=='O' and board[4]=='O' and board[5]=='O':
+            print("Well done, player 2. You won")
+            winner=True
+            break
+        elif board[6]=='X' and board[7]=='X' and board[8]=='X':
+            print("Well done, player 1. You won")
+            winner=True
+            break
+        elif board[6]=='O' and board[7]=='O' and board[8]=='O':
+            print("Well done, player 2. You won")
+            winner=True
+            break
+        elif board[0]=='X' and board[3]=='X' and board[6]=='X':
+            print("Well done, player 1. You won")
+            winner=True
+            break
+        elif board[0]=='O' and board[3]=='O' and board[6]=='O':
+            print("Well done, player 2. You won")
+            winner=True
+            break
+        elif board[1]=='X' and board[4]=='X' and board[7]=='X':
+            print("Well done, player 1. You won")
+            winner=True
+            break
+        elif board[1]=='O' and board[4]=='O' and board[7]=='O':
+            print("Well done, player 2. You won")
+            winner=True
+            break
+        elif board[2]=='X' and board[5]=='X' and board[8]=='X':
+            print("Well done, player 1. You won")
+            winner=True
+            break
+        elif board[2]=='O' and board[5]=='O' and board[8]=='O':
+            print("Well done, player 2. You won")
+            winner=True
+            break
+        elif board[0]=='X' and board[4]=='X' and board[8]=='X':
+            print("Well done, player 1. You won")
+            winner=True
+            break
+        elif board[0]=='O' and board[4]=='O' and board[8]=='O':
+            print("Well done, player 2. You won")
+            winner=True
+            break
+        elif board[2]=='X' and board[4]=='X' and board[6]=='X':
+            print("Well done, player 1. You won")
+            winner=True
+            break
+        elif board[2]=='O' and board[4]=='O' and board[6]=='O':
+            print("Well done, player 2. You won")
+            winner=True
+            break
+        else:
+            break
+
+    return winner
 
 
-board=[0,1,'X',3,'O',5,6,7,8]
+
+board=[0,1,2,3,4,5,6,7,8]
 playGame()
 
 
